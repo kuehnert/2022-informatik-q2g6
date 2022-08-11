1deadc9120013c85f43697c108c71b1e751b8a79
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-08-11 09:35:08 +0200

    Stunde vom 11.08.2022

--- src/trees/BinBaum.java
+++ src/trees/BinBaum.java
@@ -4,11 +4,23 @@ public class BinBaum {
     Knoten<Integer> wurzel;
 
     public BinBaum() {
+        // Top-down
         wurzel = new Knoten<Integer>(5);
-        wurzel.links = new Knoten(8);
-        wurzel.links.links = new Knoten<>(18);
+        wurzel.links = new Knoten<>(8, new Knoten<>(18), new Knoten<>(3));
+        wurzel.rechts = new Knoten<>(6);
+        wurzel.rechts.links = new Knoten<>(36);
+        wurzel.rechts.rechts = new Knoten<>(4, null, new Knoten<>(12));
 
-        wurzel.rechts = new Knoten(6);
+        System.out.println(wurzel.gibAus());;
+
+        // Bottom-Up
+        Knoten t8 = new Knoten<>(8, new Knoten<>(18), new Knoten<>(3));
+        Knoten t6 = null;
+        wurzel = new Knoten<>(5, t8, t6);
+    }
+
+    public static void main(String[] args) {
+        new BinBaum();
     }
 }
 
@@ -25,5 +37,22 @@ class Knoten<T> {
         this.data = data;
         this.links = links;
         this.rechts = rechts;
+    }
+
+    public String gibAus() {
+        // 1. Gib die aktuelle Zahl aus
+        String output = data.toString();
+
+        // 2. Wenn links ein Knoten ist, gib ihn aus
+        if (links != null) {
+            output += links.gibAus();
+        }
+
+        // 2. Wenn rechts ein Knoten ist, gib ihn aus
+        if (rechts != null) {
+            output += rechts.gibAus();
+        }
+
+        return output;
     }
 }
