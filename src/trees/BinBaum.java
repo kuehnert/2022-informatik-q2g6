03dd1beaf3b1e42fe430137408800b4e99973802
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-08-15 14:06:18 +0200

    Stunde vom 15.08.2022

--- src/trees/BinBaum.java
+++ src/trees/BinBaum.java
@@ -5,18 +5,37 @@ public class BinBaum {
 
     public BinBaum() {
         // Top-down
-        wurzel = new Knoten<Integer>(5);
-        wurzel.links = new Knoten<>(8, new Knoten<>(18), new Knoten<>(3));
-        wurzel.rechts = new Knoten<>(6);
-        wurzel.rechts.links = new Knoten<>(36);
-        wurzel.rechts.rechts = new Knoten<>(4, null, new Knoten<>(12));
-
-        System.out.println(wurzel.gibAus());;
+        // wurzel = ne w Knoten<Integer>(5);
+        // wurzel.links = new Knoten<>(8, new Knoten<>(18), new Knoten<>(3));
+        // wurzel.rechts = new Knoten<>(6);
+        // wurzel.rechts.links = new Knoten<>(36);
+        // wurzel.rechts.rechts = new Knoten<>(4, null, new Knoten<>(12));
 
         // Bottom-Up
         Knoten t8 = new Knoten<>(8, new Knoten<>(18), new Knoten<>(3));
-        Knoten t6 = null;
+        Knoten t4 = new Knoten<>(4, null, new Knoten<>(12));
+        Knoten t6 = new Knoten<>(6, new Knoten<>(36), t4);
         wurzel = new Knoten<>(5, t8, t6);
+
+        System.out.println(wurzel.gibAus());;
+    }
+
+    public Knoten suche(Integer gesucht) {
+        return wurzel.suche(gesucht);
+    }
+
+    public void einfuegen(Integer data) {
+        Knoten<> newKnoten = new Knoten(data);
+        Knoten<> runner = wurzel;
+
+        while (true) {
+            // Sind die Daten größer als runner.data?
+            // Fehler, weil Datentyp "T" kein ">" kann!
+            if (runner.data > data) {
+
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -39,20 +58,29 @@ class Knoten<T> {
         this.rechts = rechts;
     }
 
+    public Knoten suche(Integer gesucht) {
+        if (data.equals(gesucht)) {
+            return this;
+        }
+
+        // ....
+    }
+
     public String gibAus() {
         // 1. Gib die aktuelle Zahl aus
         String output = data.toString();
 
         // 2. Wenn links ein Knoten ist, gib ihn aus
         if (links != null) {
-            output += links.gibAus();
+            output += "[" + links.gibAus() + "]";
         }
 
         // 2. Wenn rechts ein Knoten ist, gib ihn aus
         if (rechts != null) {
-            output += rechts.gibAus();
+            output += "<" + rechts.gibAus() + ">";
         }
 
         return output;
     }
+
 }
