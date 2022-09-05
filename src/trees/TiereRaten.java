7d5393417a9c083d5f91243c7a13360ea64523ac
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-09-05 13:45:31 +0200

    Stunde vom 05.09.2022

--- src/trees/TiereRaten.java
+++ src/trees/TiereRaten.java
@@ -8,8 +8,8 @@ public class TiereRaten {
 
     public TiereRaten() {
         wurzel = new Frage("Ist es ein Säugetier?");
-        wurzel.ja = new Frage("Löwe");
-        wurzel.nein =new Frage( "Papagei");
+        wurzel.ja = new Frage("Loewe");
+        wurzel.nein = new Frage("Papagei");
     }
 
     public void spiele() {
@@ -17,9 +17,29 @@ public class TiereRaten {
         Frage runner = wurzel;
 
         // Schleife
-        System.out.println(runner.inhalt);
-        String antwort = eingabe.nextLine();
+        while (runner != null) {
+            System.out.println(runner.inhalt + " ");
+            boolean antwortJa = eingabe.nextLine().startsWith("j");
 
+            if (runner.istTier()) {
+                if (antwortJa) {
+                    System.out.println("Hurrah, ich habe das Tier geraten und damit gewonnen!");
+                } else {
+                    System.out.println("Oh je, ich bin am Ende meines Lateins. Welches Tier hast Du Dir gedacht? ");
+                    String neuesTier = eingabe.nextLine();
+                    System.out.println("Stelle eine Frage, um altes Tier und neues Tier zu unterscheiden, und für die" +
+                            " ja die Antwort für neues Tier ist. ");
+                    String neueFrage = eingabe.nextLine();
+                    System.out.println("Würg.");
+                }
+            } else {
+                if (antwortJa) {
+                    runner = runner.ja;
+                } else {
+                    runner = runner.nein;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -34,5 +54,9 @@ class Frage {
 
     public Frage(String inhalt) {
         this.inhalt = inhalt;
+    }
+
+    public boolean istTier() {
+        return ja == null; // && nein == null;
     }
 }
