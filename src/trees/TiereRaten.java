1e77cbff58dbf6dc6750cf71d24ca9534a7e979b
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-09-08 08:36:59 +0200

    Stunde vom 08.09.2022

--- src/trees/TiereRaten.java
+++ src/trees/TiereRaten.java
@@ -1,5 +1,6 @@
 package trees;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TiereRaten {
@@ -14,6 +15,16 @@ public class TiereRaten {
 
     public void spiele() {
         System.out.println("Willkommen beim Expertensystem");
+        boolean weiter = true;
+
+        while (weiter) {
+            spieleRunde();
+            System.out.print("Noch einmal spielen (j/n)? ");
+            weiter = eingabe.nextLine().toLowerCase(Locale.ROOT).startsWith("j");
+        }
+    }
+
+    public void spieleRunde() {
         Frage runner = wurzel;
         Frage pred = null;
 
@@ -27,19 +38,24 @@ public class TiereRaten {
                     System.out.println("Hurrah, ich habe das Tier geraten und damit gewonnen!");
                 } else {
                     System.out.println("Oh je, ich bin am Ende meines Lateins. Welches Tier hast Du Dir gedacht? ");
-                    String neuesTier = eingabe.nextLine();
+                    String neuesTierStr = eingabe.nextLine();
                     System.out.println("Stelle eine Frage, um altes Tier und neues Tier zu unterscheiden, und für die" +
                             " ja die Antwort für neues Tier ist. ");
-                    String neueFrage = eingabe.nextLine();
+                    String neueFrageStr = eingabe.nextLine();
 
                     // Vorsicht!
-                    pred.nein = new Frage(neueFrage);
-                    // Setze neuesTier auf linke Antwort
-                    // Setze altesTier auf rechte Antwort
+                    Frage neueFrage = new Frage(neueFrageStr);
+                    pred.nein = neueFrage;
 
+                    // Setze neuesTier auf linke Antwort
+                    neueFrage.ja = new Frage(neuesTierStr);
+
+                    // Setze altesTier auf rechte Antwort
+                    neueFrage.nein = runner;
+                    runner = null;
                 }
             } else {
-                System.out.println(runner.inhalt + " ");
+
                 boolean antwortJa = eingabe.nextLine().startsWith("j");
 
                 pred = runner;
@@ -50,6 +66,11 @@ public class TiereRaten {
                 }
             }
         }
+    }
+
+    private boolean jaNeinFrage(String prompt) {
+        System.out.print(prompt + " (j/n)?");
+        return eingabe.nextLine().toLowerCase(Locale.ROOT).startsWith("j");
     }
 
     public static void main(String[] args) {
