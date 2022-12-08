349ce98722f6125cca42d52b12b19cd19d4851f4
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-12-08 10:21:10 +0100

    Stunde vom 08.12.2022

--- src/spielundspass/Advent.java
+++ src/spielundspass/Advent.java
@@ -1,5 +1,7 @@
 package spielundspass;
 
+import java.util.Random;
+
 public class Advent {
     static double piannaehrung() {
         int v = +4;
@@ -12,7 +14,7 @@ public class Advent {
             System.out.printf("%d: %.5f %n", n, wert);
 
             int wi = (int) (wert * 100_000);
-            if (wi == pred ) {
+            if (wi == pred) {
                 break;
             } else {
                 pred = wi;
@@ -22,7 +24,40 @@ public class Advent {
         return wert;
     }
 
+    public static void monteCarlo() {
+        Random g = new Random();
+        int innen = 0, gesamt = 0;
+        double roundedPi = Math.floor(Math.PI * 10_000);
+        double pi = 0.0;
+
+        while (gesamt < 100_000) {
+            double x = g.nextDouble();
+            double y = g.nextDouble();
+
+            // berechne die Länge der Hypothenuse
+            double z = Math.sqrt(x * x + y * y);
+
+            if (z <= 1.0) {
+                innen++;
+            }
+
+            pi = 4.0 * innen / ++gesamt;
+
+            if (gesamt % 1000 == 0) {
+                System.out.println("Meine Pi-Annäherung (" + innen + "/" + gesamt + "): " + pi);
+                System.out.println(Math.abs(Math.PI - pi));
+            }
+
+            if (Math.floor(pi * 10_000) == roundedPi) {
+                break;
+            }
+        }
+
+        System.out.println("Meine Pi-Annäherung: " + pi);
+        System.out.println(Math.abs(Math.PI - pi));
+    }
+
     public static void main(String[] args) {
-        piannaehrung();
+        monteCarlo();
     }
 }
