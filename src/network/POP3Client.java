a88f1c398fc941b0da833e4d8ad6825e425a1cca
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2023-01-26 09:33:00 +0100

    Stunde vom 26.01.2023

--- src/network/POP3Client.java
+++ src/network/POP3Client.java
@@ -3,6 +3,7 @@ package network;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class POP3Client {
@@ -47,16 +48,77 @@ public class POP3Client {
         System.out.println("Hier kommt die Anmeldung hin");
         writer.println("USER " + USERNAME);
         writer.flush();
-        System.out.println("Hä?");
 
         reader.nextLine();
         String antwort = reader.nextLine();
         System.out.println("EMPFANGEN: " + antwort);
 
+        // Sende Passwort
+        writer.println("PASS " + PASSWORD);
+        writer.flush();
+
+        reader.nextLine();
+        antwort = reader.nextLine();
+        System.out.println("EMPFANGEN: " + antwort);
+
+        if (!antwort.startsWith("+OK")) {
+            // Fehlerfall
+            System.err.println("Fehler bei Benutzername/Passwort");
+            try {
+                socket.close();
+            } catch (IOException e) {
+            }
+
+            System.exit(1);
+        }
     }
 
     private void listeMails() {
-        System.out.println("Hier Arbeit für Felix und Nicht-Felixe.");
+        System.out.println("Hole Liste von E-Mails.");
+
+        writer.println("LIST"); writer.flush();
+        String antwort = reader.nextLine();
+        if (antwort.startsWith("-ERR")) {
+            System.out.println("Fehler beim Listen der Mails");
+            try {
+                socket.close();
+            } catch (IOException e) {
+            }
+            System.exit(1);
+        }
+
+        System.out.println(antwort);
+        ArrayList<Integer> idListe = new ArrayList<>();
+
+        while (true) {
+            antwort = reader.nextLine();
+
+            if (antwort.equals("")) {
+                // starte nächsten Schleifendurchlauf
+                continue;
+            } else if (antwort.equals(".")) {
+                // breche die Schleife ab
+                break;
+            }
+
+            // Wir bekommen eine Zeile der Form "15 45378437"
+            String idString = antwort.split(" ")[0];
+            int id = Integer.parseInt(idString);
+            idListe.add(id);
+
+            System.out.println("EMPFANGEN: " + antwort);
+        }
+
+        // SAFE
+        System.out.println("IDs: " + idListe);
+
+        for (int id: idListe) {
+            holeMail(id);
+        }
+    }
+
+    private void holeMail(int id) {
+        System.out.println("Hole Mail mit ID " + id);
     }
 
     private void beendeVerbindung() {
@@ -71,9 +133,5 @@ public class POP3Client {
 
     public static void main(String[] args) {
         new POP3Client();
-        // String input = reader.nextLine();
-        // botschaft.startsWith("+")
-        // writer.println(botschaft);
-        // writer.flush();
     }
 }
