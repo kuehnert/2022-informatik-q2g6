21e81e72d67048333c89170367c5d477c5a69773
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2023-02-02 10:47:57 +0100

    Stunde vom 02.02.2023

--- src/threading/Spiel.java
+++ src/threading/Spiel.java
@@ -1,5 +1,6 @@
 package threading;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Spiel {
@@ -15,17 +16,28 @@ public class Spiel {
         Buchstabenwerfer werferin = new Buchstabenwerfer();
         punkte = 0;
 
+        System.out.println("Willkommen beim Tipp-Spiel, los geht's!");
+        werferin.start();
+
         for (int i = 0; i < 10; i++) {
             String eingabe = tastatur.nextLine();
-            if (eingabe.charAt(0) == werferin.buchstabe) {
+
+            if (werferin.buchstabe == Buchstabenwerfer.LEERZEICHEN) {
+                System.out.println("Zu spät!");
+            } else if (eingabe.length() > 0 && eingabe.charAt(0) == werferin.buchstabe) {
                 System.out.println("Toll, richtig!");
                 // Teile Werferin mit, dass Buchstabe eingegeben wurde
+            } else {
+                System.out.println("Falsch!");
             }
         }
+
+        werferin.stop();
+        System.out.println("Ende vom Tipp-Spiel");
     }
 
     public static void main(String[] args) {
-        new Spiel();
+        new Spiel().spielen();
     }
 }
 
@@ -34,11 +46,36 @@ public class Spiel {
  * den der User drücken muss. Dafür hat eine Sekunde Zeit
  */
 class Buchstabenwerfer extends Thread {
-    public char buchstabe = '_';
+    public static final char LEERZEICHEN = '\00';
+    private static Random gen = new Random();
+    public char buchstabe = LEERZEICHEN;
+
+    private void schlafe(int sekunden) {
+        try {
+            sleep(sekunden * 1000);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    private void schlafe(int min, int max) {
+        try {
+            sleep(gen.nextInt(max - min) + min);
+        } catch (InterruptedException e) {
+        }
+    }
 
     public void run() {
         // Endlosschleife
-        // Alle 2-6 Sekunden setze neuen Buchstaben
-        // Nach einer Sekunde, setze den Buchstaben auf '😎'
+        while (true) {
+            // Alle 2-6 Sekunden setze neuen Buchstaben
+            schlafe(2, 6);
+
+            buchstabe = (char) ('a' + gen.nextInt(26));
+            System.out.println("BUCHSTABE: " + buchstabe);
+
+            // Nach einer Sekunde, setze den Buchstaben auf '😎'
+            schlafe(1);
+            buchstabe = LEERZEICHEN;
+        }
     }
 }
